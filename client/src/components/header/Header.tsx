df6da4eb1ee3923da8814bf76208433a79e2f09a
marcusmariette <102576833@student.swin.edu.au>
2022-05-22 13:52:05 +1000

    [Marcus]-[Added Cart Icon and Navigation]

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,4 +1,5 @@
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Box } from '@mui/system';
 import React from 'react';
 
@@ -6,7 +7,7 @@ const Header: React.FC = () => {
     return (
         <>
             <Box>
-                <AppBar position='fixed' color={'secondary'}>
+                <AppBar position='fixed' color={'secondary'} sx={{ paddingLeft: '25px', paddingRight: '25px' }}>
                     <Toolbar>
                         <Typography
                             variant='h6'
@@ -16,10 +17,14 @@ const Header: React.FC = () => {
                                 mr: 2,
                                 color: 'inherit',
                                 textDecoration: 'none',
+                                flex: 1,
                             }}
                         >
                             Cosy Kangaroo
                         </Typography>
+                        <IconButton aria-label='cartIcon' href='/cart' sx={{ color: '#fff' }}>
+                            <ShoppingCartIcon fontSize='large' />
+                        </IconButton>
                     </Toolbar>
                 </AppBar>
             </Box>
