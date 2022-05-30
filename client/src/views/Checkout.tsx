177b707744d9b8db939c4d02058e6924980e2264
cafraaa <48873871+cafraaa@users.noreply.github.com>
2022-05-30 21:51:00 +1000

    [Michael]-[Friday Changes]

--- client/src/views/Checkout.tsx
+++ client/src/views/Checkout.tsx
@@ -1,12 +1,39 @@
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, Typography, TextField } from '@mui/material';
+import { textAlign } from '@mui/system';
 
 const Checkout = () => {
     return (
         <Container disableGutters maxWidth={false}>
-            <Box height={'100vh'} display={'flex'} flexDirection={'column'}>
+            <Box display={'flex'} flexDirection={'column'}>
                 <Typography variant='h2' align='center' color='primary.contrastText' gutterBottom paddingTop={20}>
                     Checkout
                 </Typography>
+            </Box>
+
+            <Box component="form" sx={{'& .MuiTextField-root': { m: 1, width: '25ch', alignItems: 'center' },}}>
+                <div >
+                    <TextField 
+                        
+                        defaultValue="Email"    
+                        variant="filled"
+                    />
+                </div>
+                <div>
+                    <TextField
+                        defaultValue="First name"
+                        variant="filled"
+                    />
+                    <TextField
+                        defaultValue="Last name"
+                        variant="filled"
+                    />
+                </div>
+                <div>
+                    <TextField
+                        defaultValue="Mobile"
+                        variant="filled"
+                    />
+                </div>
             </Box>
         </Container>
     );
