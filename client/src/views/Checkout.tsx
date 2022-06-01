40471cd2583c2f09dcc5d11d6267024424566bc9
cafraaa <48873871+cafraaa@users.noreply.github.com>
2022-06-01 22:23:24 +1000

    [Michael]-[Checkout Changes]

--- client/src/views/Checkout.tsx
+++ client/src/views/Checkout.tsx
@@ -1,39 +1,15 @@
 import { Box, Container, Typography, TextField } from '@mui/material';
 import { textAlign } from '@mui/system';
+import CheckoutForm from 'components/checkoutForm/CheckoutForm';
 
 const Checkout = () => {
     return (
-        <Container disableGutters maxWidth={false}>
-            <Box display={'flex'} flexDirection={'column'}>
-                <Typography variant='h2' align='center' color='primary.contrastText' gutterBottom paddingTop={20}>
+        <Container disableGutters maxWidth={false}>     
+            <Box height={'100vh'} display={'flex'} flexDirection={'column'}>
+                <Typography variant='h2' align='center' color='primary.contrastText' gutterBottom paddingTop={20} paddingBottom={10}>
                     Checkout
                 </Typography>
-            </Box>
-
-            <Box component="form" sx={{'& .MuiTextField-root': { m: 1, width: '25ch', alignItems: 'center' },}}>
-                <div >
-                    <TextField 
-                        
-                        defaultValue="Email"    
-                        variant="filled"
-                    />
-                </div>
-                <div>
-                    <TextField
-                        defaultValue="First name"
-                        variant="filled"
-                    />
-                    <TextField
-                        defaultValue="Last name"
-                        variant="filled"
-                    />
-                </div>
-                <div>
-                    <TextField
-                        defaultValue="Mobile"
-                        variant="filled"
-                    />
-                </div>
+                <CheckoutForm/>
             </Box>
         </Container>
     );
