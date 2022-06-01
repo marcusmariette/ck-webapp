5c5e3195f5c3dfc249625c7be82a3ff4e9d964b7
BrankoRoknic <91444027+BrankoRoknic@users.noreply.github.com>
2022-06-01 10:56:06 +1000

    Requested Changes.
    As requested:
    - random naming conventions amended - code formatting

--- client/src/views/BookingSuccess.tsx
+++ client/src/views/BookingSuccess.tsx
@@ -1,32 +1,44 @@
-import { Box, Container, Typography } from '@mui/material';
-import CenteredButton from 'components/button/CenteredButton';
+import { Box, Container, Typography } from "@mui/material";
+import CenteredButton from "components/button/CenteredButton";
 
-const Home = () => {
-    let message;
+const BookingSuccess = () => {
+  let message;
 
-    if (localStorage.getItem('message')== null){
-        message = '';
-        localStorage.setItem('message', JSON.stringify(message))
-    }else{
-        message = JSON.parse(localStorage.getItem('message')!);
-    }
+  if (localStorage.getItem("message") == null) {
+    message = "";
+    localStorage.setItem("message", JSON.stringify(message));
+  } else {
+    message = JSON.parse(localStorage.getItem("message")!);
+  }
 
-    return (
-        <Container disableGutters maxWidth={false}>
-            <Box height={'100vh'} display={'flex'} flexDirection={'column'}>
-                <Typography variant='h2' align='center' color='primary.contrastText' gutterBottom paddingTop={20}>
-                    Success!
-                </Typography>
-                <Typography variant='h5' align='center' color='primary.contrastText' paddingBottom={10} paddingTop={5}>
-                    {message}
-                </Typography>
-                <Box>
-                    <CenteredButton text={'Return to Homepage'} link={'/'} />
-                    <CenteredButton text={'Make Another Booking'} link={'/booking'} />
-                </Box>
-            </Box>
-        </Container>
-    );
+  return (
+    <Container disableGutters maxWidth={false}>
+      <Box height={"100vh"} display={"flex"} flexDirection={"column"}>
+        <Typography
+          variant="h2"
+          align="center"
+          color="primary.contrastText"
+          gutterBottom
+          paddingTop={20}
+        >
+          Success!
+        </Typography>
+        <Typography
+          variant="h5"
+          align="center"
+          color="primary.contrastText"
+          paddingBottom={10}
+          paddingTop={5}
+        >
+          {message}
+        </Typography>
+        <Box>
+          <CenteredButton text={"Return to Homepage"} link={"/"} />
+          <CenteredButton text={"Make Another Booking"} link={"/booking"} />
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
-export default Home;
+export default BookingSuccess;
